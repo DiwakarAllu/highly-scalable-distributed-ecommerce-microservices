096545d00c7344bae08f3ebf5804c64de2dee14c
Allu <diwakar.allu.3435@gmail.com>
2025-12-16 21:48:16 +0530

    refactor(order): add response DTO and split service interface from implementation

---
+++ order-service/src/main/java/com/diwakarallu/ecommerce/order/service/OrderServiceImpl.java
@@ -0,0 +1,88 @@
+package com.diwakarallu.ecommerce.order.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.diwakarallu.ecommerce.order.client.InventoryClient;
+import com.diwakarallu.ecommerce.order.dto.OrderRequest;
+import com.diwakarallu.ecommerce.order.dto.OrderResponse;
+import com.diwakarallu.ecommerce.order.event.OrderPlacedEvent;
+import com.diwakarallu.ecommerce.order.exception.ProductOutOfStockException;
+import com.diwakarallu.ecommerce.order.model.Order;
+import com.diwakarallu.ecommerce.order.repository.OrderRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+
+            // Thread.sleep(5000);
+            var order = mapToOrder(orderRequest);
+            orderRepository.save(order);
+
+            // send the msg to kafka topic - order-placed
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+
+            log.info("Start - Sending OrderPlacedEvent to Kafka Topic: order-placed for Order Number: {}",
+                    order.getOrderNumber());
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("Completed - Sending OrderPlacedEvent to Kafka Topic: order-placed for Order Number: {}",
+                    order.getOrderNumber());
+
+            return mapToOrderResponse(order);
+        } else {
+            throw new ProductOutOfStockException(
+                    "Product with SKU Code " + orderRequest.skuCode() + "and quantity " + orderRequest.quantity()
+                            + " is not available in inventory.");
+        }
+    }
+
+    public List<OrderResponse> getAllOrders() {
+        List<Order> orders = orderRepository.findAll();
+        return orders.stream().map(this::mapToOrderResponse).toList();
+    }
+
+    public OrderResponse getOrderDetail(String orderNumber) {
+        return mapToOrderResponse(orderRepository.findByOrderNumber(orderNumber));
+    }
+
+    public void cancelOrder(String orderNumber) {
+        orderRepository.deleteByOrderNumber(orderNumber);
+    }
+
+    private OrderResponse mapToOrderResponse(Order order) {
+        return new OrderResponse(order.getId(), order.getOrderNumber(), order.getSkuCode(), order.getPrice(),
+                order.getQuantity());
+    }
+
+    private static Order mapToOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+        order.setQuantity(orderRequest.quantity());
+        order.setSkuCode(orderRequest.skuCode());
+        return order;
+    }
+}
